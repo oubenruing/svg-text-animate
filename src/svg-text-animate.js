68f16065cfe0be4c03fb0acb79a7015518acfe03
oubenruing <rui.chen@bluedoordigital.cn>
2019-11-04 18:21:17 +0800

    update
    First version can be used

--- src/svg-text-animate.js
+++ src/svg-text-animate.js
@@ -1,9 +1,4 @@
-// var opentype = require('opentype.js');
-// var makerjs = require('makerjs');
-// var Vivus = require('vivus');
 import * as opentype from 'opentype.js';
-import Vivus from "vivus"
-
 export default class SVGTextAnimate {
   constructor(fontfile, fontsize) {
     this.fontfile = fontfile;
@@ -38,37 +33,64 @@ export default class SVGTextAnimate {
     return { x1: 0, y1: 0, x2, y2 }
   }
 
-  animate(svgDom, options) {
-    const _options={
-
+  animatePath(svgDom, options) {
+    const _options = {
+      "duration": 1000,
+      "timing-function": "linear",
+      "iteration-count": 1,
+      "direction": "normal",
+      "fill-mode": "forwards",
+      "delay": 0,
+      "mode": "sync"
     }
+    Object.assign(_options, options);
+
     let style = svgDom.querySelector("style");
     const paths = svgDom.querySelectorAll("path");
 
     if (style != null) {
       style.innerHTML = "";
     }
-    else{
+    else {
       style = document.createElement("style");
+      style.innerHTML = "@keyframes STAdraw{to{stroke-dashoffset:0}}"
       svgDom.appendChild(style);
     }
 
-    paths.forEach((path)=>{
-      path.getTotalLength();
+
+    paths.forEach((path, i) => {
+      const pathLength = Math.ceil(path.getTotalLength());
+      const stroke = `stroke-dasharray:${pathLength - 1} ${pathLength + 1};stroke-dashoffset:${pathLength};`
+      let animation = ""
+      switch (_options.mode) {
+        case "sync": animation = `animation: STAdraw ${_options["duration"]}ms ${_options["timing-function"]} 0ms ${_options["fill-mode"]} ${_options["iteration-count"]}`; break;
+        case "delay": animation = `animation: STAdraw ${_options["duration"]}ms ${_options["timing-function"]} ${_options.delay * i}ms ${_options["fill-mode"]} ${_options["iteration-count"]}`; break;
+        case "onebyone": animation = `animation: STAdraw ${_options["duration"]}ms ${_options["timing-function"]} ${_options["duration"] * i}ms ${_options["fill-mode"]} ${_options["iteration-count"]}`; break;
+      }
+      path.style.cssText = stroke + animation
+      console.log(stroke + animation);
     })
-    
-    let keyframes = "@keyframes draw{to{stroke-dashoffset:0}}"
   }
 
-  create(text, selector, duration, options) {
+  create(text, selector, options, style) {
     const _this = this
     const paths = _this.font.getPaths(text, 0, _this.fontsize, _this.fontsize);
     const box = _this.getBoundingBox(paths);
-    const svg = `<svg width="${box.x2 - box.x1}" height="${box.y2 - box.y1}" viewBox="${box.x1} ${box.y1} ${box.x2} ${box.y2}" xmlns="http://www.w3.org/2000/svg"><g id="svgGroup" stroke-linecap="round" fill-rule="evenodd" font-size="72px" stroke="#000" stroke-width="1px" fill="none" style="stroke:#000;stroke-width:1px;fill:none"></g></svg>`
+    console.log(box);
+    const _style = {
+      "stroke": "#000",
+      "stroke-width": "1px",
+    }
+    Object.assign(_style, style);
+    const end = _style["stroke-width"].search(/[A-Za-z]+$/);
+    const strokeWidth = Number(_style["stroke-width"].substring(0, end))
+    const svg = `<svg width="${box.x2 - box.x1 + strokeWidth / 2}" height="${box.y2 - box.y1}" viewBox="${box.x1} ${box.y1} ${box.x2 + strokeWidth / 2} ${box.y2 + strokeWidth / 2}" xmlns="http://www.w3.org/2000/svg">\
+    <g id="svgGroup" stroke-linecap="round" fill-rule="evenodd" font-size="72px" stroke="#000" stroke-width="1px" fill="none" style="fill:none; stroke:${_style.stroke};stroke-width:${_style["stroke-width"]}"></g>\
+    </svg>`
     const fatherdom = document.querySelector(selector);
-    fatherdom.innerHTML = svg
-
-    const svgDom = fatherdom.querySelector("svg");
+    const _div = document.createElement("div")
+    _div.innerHTML = svg
+    const svgDom = _div.querySelector("svg");
     const group = svgDom.querySelector("g");
 
     let svgpath = "";
@@ -76,8 +98,7 @@ export default class SVGTextAnimate {
       svgpath += path.toSVG(2)
     });
     group.innerHTML = svgpath;
-    _this.animate(svgDom);
-    // _this.controllers.push(new Vivus(svgDom, { duration: duration || 480, type: "delayed" }));
-    // return _this.controllers[_this.controllers.length - 1]
+    _this.animatePath(svgDom, options);
+    fatherdom.innerHTML = _div.innerHTML;
   }
 }
